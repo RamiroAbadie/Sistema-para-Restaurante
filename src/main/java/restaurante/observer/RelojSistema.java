1ae5e95aa1ee501ccb41d3b89584b0cbe14970a3
Ramiro Abadie <111799181+RamiroAbadie@users.noreply.github.com>
2025-06-13 04:20:04 -0300

    Minor FIX
    -Hice privado un atributo
    
    Co-Authored-By: Emir Gerbasi <118369865+emirgerbasi@users.noreply.github.com>

--- src/main/java/restaurante/observer/RelojSistema.java
+++ src/main/java/restaurante/observer/RelojSistema.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class RelojSistema {
     private static RelojSistema instancia;
     private List<PedidoObserver> observadores = new ArrayList<>();
-    ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     private RelojSistema() {
         // Cada 1 segundos
